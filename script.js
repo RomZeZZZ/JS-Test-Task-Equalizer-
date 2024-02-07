6b6e86f72ea71303c8bf692189bc382be918fd9d
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2024-02-07 12:58:25 +0100

    i screwed up. didn't make it on time :(

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 'use strict'
 let input = document.getElementById('input')
 let audioContainer = document.getElementById('audio-container');
+
+// function colorizeColumns(value) {
+//     const gridCells = document.querySelectorAll('.grid-cell');
+//     const columns = 6;
+//     for (let col = 0; col < columns; col++) {
+//         const height = Math.floor(value / 255 * 6); 
+//         for (let row = 0; row < 6; row++) {
+//             const cellIndex = row * columns + col;
+//             const cell = gridCells[cellIndex];
+//             const backgroundColor = row < height ? `rgba(0, 0, 255, ${value / 255})` : '';
+//             cell.style.backgroundColor = backgroundColor;
+//         }
+//     }
+// }
 input.addEventListener('change', () => {
     if (input.files[0].type.startsWith('audio/')) {
         let audioPlayer = document.createElement('audio');
@@ -11,6 +25,27 @@ input.addEventListener('change', () => {
         audioPlayer.appendChild(source);
         audioContainer.innerHTML = '';
         audioContainer.appendChild(audioPlayer);
+        // const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        // const analyser = audioContext.createAnalyser();
+        // const sourceNode = audioContext.createMediaElementSource(audioPlayer);
+        // sourceNode.connect(analyser);
+        // analyser.connect(audioContext.destination);
+        // analyser.fftSize = 32;
+        // const bufferLength = analyser.frequencyBinCount;
+        // const dataArray = new Uint8Array(bufferLength);
+        // function updateVisualizer() {
+        //     analyser.getByteFrequencyData(dataArray);
+        //     dataArray.forEach((value, index) => {
+        //         const scale = value / 255; 
+        //         const minScale = 0.2; 
+        //         const maxScale = 1.0; 
+        //         const height = Math.floor(minScale + scale * (maxScale - minScale) * 6);
+        //         colorizeColumns(value)
+        //         requestAnimationFrame(updateVisualizer);
+        //     });
+        //     requestAnimationFrame(updateVisualizer);
+        // }
+        // updateVisualizer();
     } else {
         alert('Its not audio file')
     }
