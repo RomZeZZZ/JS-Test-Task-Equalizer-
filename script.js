3d51aecc04da4620368427bbb008999476ea9a97
RomZeZZZ <romzezzz.nevdah@yandex.ru>
2024-02-07 23:14:24 +0100

    fix: several improvements

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 'use strict'
 let input = document.getElementById('input')
 let audioContainer = document.getElementById('audio-container');
-let coloumn = 1;
-let isAudioPlaying = false;
-function colorizeColumns(height, coloumn) {
+let column = 1;
+let animationId;
+function colorizeColumns(height, column) {
     if (height === 0) {
-        return
+        return;
     }
-    for (let i = 1; i <= height; i++) {
-        const cellId = `cell-${i}-${coloumn}`;
+    for (let i = height; i >= 1; i--) {
+        const cellId = `cell-${i}-${column}`;
         const cell = document.getElementById(cellId);
-        cell.style.backgroundColor = 'red';
-        setTimeout(() => {
-            cell.style.backgroundColor = '';
-        }, 100);
+        cell.style.backgroundColor = 'lightgreen';
     }
+    setTimeout(() => {
+        for (let i = 1; i <= height; i++) {
+            const cellId = `cell-${i}-${column}`;
+            const cell = document.getElementById(cellId);
+            cell.style.backgroundColor = '';
+        }
+    }, 10);
 }
-input.addEventListener('change', () => {
+input.addEventListener('change', (e) => {
+    document.querySelector('.custom-input').innerText = e.target.files[0].name || 'Choose an audio file';
     let audioPlayer = document.createElement('audio');
     audioPlayer.controls = true;
     let source = document.createElement('source');
@@ -25,7 +30,7 @@ input.addEventListener('change', () => {
     audioPlayer.appendChild(source);
     audioContainer.innerHTML = '';
     audioContainer.appendChild(audioPlayer);
-    const audioContext = new (window.AudioContext || window.AudioContext)();
+    const audioContext = new (window.AudioContext)();
     const analyser = audioContext.createAnalyser();
     const sourceNode = audioContext.createMediaElementSource(audioPlayer);
     sourceNode.connect(analyser);
@@ -34,27 +39,20 @@ input.addEventListener('change', () => {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
     audioPlayer.addEventListener('play', () => {
-        isAudioPlaying = true
-        requestAnimationFrame(updateVisualizer);
-    })
+        updateVisualizer();
+    });
     audioPlayer.addEventListener('pause', () => {
-        isAudioPlaying = false
-        cancelAnimationFrame(updateVisualizer);
-    })
+        cancelAnimationFrame(animationId);
+    });
     function updateVisualizer() {
-        requestAnimationFrame(updateVisualizer);
+        animationId = requestAnimationFrame(updateVisualizer);
         analyser.getByteFrequencyData(dataArray);
         dataArray.forEach((value) => {
-            const scale = value / 255; 
-            const minScale = 0.2; 
-            const maxScale = 1.0; 
-            const height = Math.floor(minScale + scale * (maxScale - minScale) * 6);
-            if (coloumn === 6) {
-                coloumn = 1
-            } else {
-                coloumn++
-            } 
-            colorizeColumns(height, coloumn)
+            const scale = value / 255;
+            const height = Math.round(scale * 6);
+            if (column === 6) column = 1;
+            else column++;
+            colorizeColumns(height, column);
         });
     }
 })
